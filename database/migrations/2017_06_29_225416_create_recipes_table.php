4069ee534bd6b9b8057922f5a4490b3533a51d22
Israel Ponce <Israel Ponce>
2017-06-29 19:05:24 -0400

    recipe table done

---
+++ database/migrations/2017_06_29_225416_create_recipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('title');
+            $table->string('ingredients');
+            $table->string('instructions');
+            $table->integer('servings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
